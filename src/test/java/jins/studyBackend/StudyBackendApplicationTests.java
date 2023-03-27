434afcb30bfc27e1187faa20604bef591cfd625e
YangJinSoo94 <jschrome782@gmail.com>
2023-03-27 10:19:52 +0900

    first commit

---
+++ src/test/java/jins/studyBackend/StudyBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package jins.studyBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
